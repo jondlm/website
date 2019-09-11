9bba524096c15474470d5f398ffe947bc63da17f
Jon de la Motte <jon.de.la.motte@xandr.com>
2019-09-10 22:47:18 -0700

    Bit of a restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        sm: [{ raw: "print" }, { min: "640px" }],
+        md: [{ raw: "print" }, { min: "768px" }],
+        lg: [{ raw: "print" }, { min: "1024px" }],
+        xl: [{ min: "1280px" }]
+      }
+    }
   },
   variants: {},
   plugins: []
-}
+};
