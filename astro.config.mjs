cd864bd6ea136c5deab515b2047aa48a97f857a0
Jon de la Motte <jondlm@gmail.com>
2024-10-19 09:22:33 -0700

    working_on_deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://jondlm.github.io/website/",
+  base: "website",
+  integrations: [tailwind()],
 });
